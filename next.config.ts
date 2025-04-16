21119edaa9acba57747aac8ed5f72f3bb9569eee
katsu1101 <ksasaki1101@gmail.com>
2025-04-17 00:45:11 +0900

    画像読み込みを一部だけに、deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/trumps-game',
+  assetPrefix: '/trumps-game/',
+  images: {
+    unoptimized: true, // next/image を使っても export に対応させる
+  },
 };
 
 export default nextConfig;
